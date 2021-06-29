abc3137b3a08f203546446f379ce9e7e47832ec0
Sergio Juarez <sergiojc90@gmail.com>
2021-06-28 23:56:04 -0500

    Importing components to main app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from "react"
+import Education from "./components/Education"
+import EmploymentHistory from "./components/EmploymentHistory"
+import PersonalDetails from "./components/PersonalDetails"
+import Preview from "./components/Preview"
+import ProfessionalSumary from "./components/ProfessionalSummary"
+import Skills from "./components/Skills"
+import Websites from "./components/Websites"
+
 
 function App() {
   return (
